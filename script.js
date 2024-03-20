8c1f1442efa8a4b9a68302e217c7005783522161
Damian Swarts <damianswarts2019@gmail.com>
2024-03-20 14:20:49 +0200

    Added functions for the basic functionality of the calculator and updated some of the CSS and HTML to better the styling of the page

--- script.js
+++ script.js
@@ -1,6 +1,46 @@
-const display = document.getElementById("display");
+//Variables for each of the parts of a calculator operation
+let firstNumber;
+let operation;
+let secondNumber;
 
-// Function to display the input of the user
-function displayValue(input){
-    display.value += input;
+// Function to add two numbers
+function add(num1, num2){
+    return num1 + num2
+}
+
+// Function to subtract two numbers
+function subtract(num1, num2){
+    return num1 - num2
+}
+
+// Function to multiply two numbers
+function multiply(num1, num2){
+    return num1 * num2
+}
+
+// Function to divide two numbers
+function divide(num1, num2){
+    return num1 / num2
+}
+
+/*Function that takes an operator and 2 numbers 
+and then calls one of the above functions on the numbers*/
+function operate(operator, num1, num2) {
+    
+    switch (operator) {
+        case '+':
+            return add(num1, num2);
+            
+        case '-':
+            return substract(num1, num2);
+    
+        case '*':
+            return multiply(num1, num2);
+            
+        case '/':
+            return divide(num1, num2);
+            
+        default:
+            return ERROR;
+    }
 }
